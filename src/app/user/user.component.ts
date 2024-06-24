c4665ec0899b153e674aab8e265d770416bea4ca
Rampratap Kasala <53807003+krampratap@users.noreply.github.com>
2024-06-24 13:33:42 +0530

    ng content and view child

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,11 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-user',
-  templateUrl: './user.component.html'
+  templateUrl: './user.component.html',
+  styles : [`
+  .offline {
+    color: white;
+    }
+  `,]
 })
 export class UserComponent {
   //Interpolation
   userId: number = 10;
   userStatus: string = 'Offline';
+
+@Input()
+userName: string='';
+  constructor(){
+    this.userStatus = Math.random() > 0.5 ? 'Offline' : 'Online'; //If Offline then styles is applied through ngClass
+    }
+
+  getColor(){
+    if(this.userStatus=='Online')
+      {
+        return 'green';
+      }
+    return 'red';
+    }
+
+
+
 }
--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -1,16 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild , ElementRef, } from '@angular/core';
 
 @Component({
   selector: 'app-users',
   templateUrl: './users.component.html',
-  styleUrls: ['./users.component.css']
-})
+  styleUrls: ['./users.component.css'],
+  //encapsulation: ViewEncapsulation.None
+   //If we define any css at this component level .. it will be applied to the entire application if we remove Encapsulation
+})  //We can remove encapsulation and define in global.. it will be applied to all components
 export class UsersComponent {
   //Property Binding
   toggleButton : boolean = true;
   userCreatedStatus : string = 'No user created';
   userName : string = '';
   userPlace : string = '';
+    userList: string[] =[];
+    @ViewChild('userAddress') userAddress: any ; //@ViewChild is used to get the DOM element
   constructor() {
     setTimeout(() => {
       this.toggleButton = false;
@@ -28,4 +32,14 @@ export class UsersComponent {
       this.userName = (event.target as HTMLInputElement).value;
       //The event can be of any type i.e button event or any other.. so for Input we typecase as  HTMLInputElement
     }
+
+
+  //Communication
+    addUser(){
+      this.userList.push(this.userName);
+      console.log(this.userList);
+      console.log(this.userAddress.nativeElement.value);
+      }
+
+
 }
